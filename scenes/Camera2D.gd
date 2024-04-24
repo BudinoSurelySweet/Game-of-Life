56c888373f5f231fd412dfc7c8c96462dc4b2944
Marco Molossi <molossi.marco1906@gmail.com>
2024-04-24 19:54:46 +0200

    Rafined the scene added camera movement and zoom, changed and rafined the cells script

---
+++ scenes/Camera2D.gd
@@ -0,0 +1,49 @@
+extends Camera2D
+
+class_name PanningCamera2D
+
+const MIN_ZOOM: float = 0.5
+const MAX_ZOOM: float = 2.5
+const ZOOM_RATE: float = 8.0
+const ZOOM_INCREMENT: float = 0.5
+
+var _target_zoom: float = 1.0
+var disable_camera_movement: bool = false
+
+
+func _physics_process(delta: float) -> void:
+	zoom = lerp(zoom, _target_zoom * Vector2.ONE, ZOOM_RATE * delta)
+	set_physics_process(not is_equal_approx(zoom.x, _target_zoom))
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if not disable_camera_movement:
+		if event is InputEventMouseButton:
+			if event.is_pressed():
+				if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+					zoom_out()
+				if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+					zoom_in()
+				if event.double_click:
+					focus_position(get_global_mouse_position())
+		if event is InputEventMouseMotion:
+			if event.button_mask == MOUSE_BUTTON_MASK_MIDDLE:
+				if zoom < Vector2(1.5, 1.5):
+					position -= event.relative * zoom / 1
+				else:
+					position -= event.relative * zoom / 3
+
+
+func zoom_in() -> void:
+	_target_zoom = max(_target_zoom - ZOOM_INCREMENT, MIN_ZOOM)
+	set_physics_process(true)
+
+
+func zoom_out() -> void:
+	_target_zoom = min(_target_zoom + ZOOM_INCREMENT, MAX_ZOOM)
+	set_physics_process(true)
+
+
+func focus_position(target_position: Vector2) -> void:
+	var _tween = get_tree().create_tween()
+	_tween.tween_property(self, "position", target_position, 0.2)
--- scenes/Cells.gd
+++ scenes/Cells.gd
@@ -1,17 +1,17 @@
 extends TileMap
 
-var map : int = 32
+const DEAD_CELL_ID : int = 0
+const LIVING_CELL_ID : int = 1
+
+var map : Vector2i = Vector2i(32, 18)
+var _cells_to_check : Dictionary
 var _next_generation_cells : Dictionary
+
 var _edit_mode : bool = true
 var _mouse_left_click_pressed : bool = false
 var _mouse_right_click_pressed : bool = false
-var _prev_mouse_pos : Vector2i
-var _mouse_pos : Vector2i : 
-	set(new_value):
-		_mouse_pos = new_value
-		
-		if _prev_mouse_pos != _mouse_pos:
-			_prev_mouse_pos = _mouse_pos
+
+var _mouse_pos : Vector2i
 
 @onready var next_generation_timer : Timer = $NextGeneration
 
@@ -19,9 +19,9 @@ var _mouse_pos : Vector2i :
 func _ready():
 	next_generation_timer.connect("timeout", _next_generation)
 	
-	for x in map:
-		for y in map:
-			set_cell(0, Vector2i(x, y), false, Vector2i(0, 0))
+	for x in map.x:
+		for y in map.y:
+			set_cell(0, Vector2i(x, y), DEAD_CELL_ID, Vector2i(0, 0))
 
 
 func _input(event):
@@ -31,13 +31,16 @@ func _input(event):
 	if _edit_mode:
 		next_generation_timer.stop()
 		
+		# click mouse button
 		if event is InputEventMouseButton:
 			if event.is_pressed():
+				# fai nacere la cella selezionata
 				if event.button_index == MOUSE_BUTTON_LEFT:
 					_mouse_left_click_pressed = true
 					
 					_born()
 				
+				# fai morire la cella selezionata
 				if event.button_index == MOUSE_BUTTON_RIGHT:
 					_mouse_right_click_pressed = true
 					
@@ -50,33 +53,37 @@ func _input(event):
 				if event.button_index == MOUSE_BUTTON_RIGHT:
 					_mouse_right_click_pressed = false
 		
+		# hold mouse button
 		if event is InputEventMouseMotion:
+			# fai nacere la cella selezionata
 			if _mouse_left_click_pressed:
 				_born()
 			
+			# fai morire la cella selezionata
 			if _mouse_right_click_pressed:
 				_death()
 	else:
-		next_generation_timer.start()
+		if next_generation_timer.time_left == 0:
+			next_generation_timer.start()
 
 
 func _born() -> void:
 	# get the mouse global position without the decimal number
 	_mouse_pos = Vector2i(get_global_mouse_position())
 	
-	set_cell(0, _mouse_pos, true, Vector2i(0, 0))
+	set_cell(0, _mouse_pos, LIVING_CELL_ID, Vector2i(0, 0))
 
 
 func _death() -> void:
 	# get the mouse global position without the decimal number
 	_mouse_pos = Vector2i(get_global_mouse_position())
 	
-	set_cell(0, _mouse_pos, false, Vector2i(0, 0))
+	set_cell(0, _mouse_pos, DEAD_CELL_ID, Vector2i(0, 0))
 
 
 func _next_generation() -> void:
-	for x in map:
-		for y in map:
+	for x in map.x:
+		for y in map.y:
 			var curr_cell_status = get_cell_source_id(0, Vector2i(x, y))
 			var adjacent_cells_pos : Array = [
 				Vector2i(x-1, y-1),
@@ -95,25 +102,26 @@ func _next_generation() -> void:
 				if get_cell_source_id(0, cell_pos) == 1:
 					adjacent_living_cells += 1
 			
+			# rules
 			if curr_cell_status:
 				if adjacent_living_cells < 2 or adjacent_living_cells > 3:
-					# la cella muore
+					# la cella muore per effetto di isolamento (<2) o per effetto di sovrappopolazione (>3)
 					_next_generation_cells[str(Vector2i(x, y))] = {
 						"Position" : Vector2i(x, y), 
-						"Life" : false
+						"Life" : DEAD_CELL_ID
 					}
 				elif adjacent_living_cells == 2 or adjacent_living_cells == 3:
 					# la cella vive
 					_next_generation_cells[str(Vector2i(x, y))] = {
 						"Position" : Vector2i(x, y), 
-						"Life" : true
+						"Life" : LIVING_CELL_ID
 					}
 			else:
 				if adjacent_living_cells == 3:
-					# la cella nasce
+					# la cella nasce per effetto di riproduzione
 					_next_generation_cells[str(Vector2i(x, y))] = {
 						"Position" : Vector2i(x, y), 
-						"Life" : true
+						"Life" : LIVING_CELL_ID
 					}
 	
 	for cell in _next_generation_cells:
